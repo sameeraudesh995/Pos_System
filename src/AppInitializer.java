2959e9fe5441ec8cade5769f874779ccc0e52790
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-07 10:54:49 +0530

    main libraries and main class added

---
+++ src/AppInitializer.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AppInitializer extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent parent = FXMLLoader.load(this.getClass().getResource("./view/AddItemForm.fxml"));
+        Scene scene = new Scene(parent);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+
+}
