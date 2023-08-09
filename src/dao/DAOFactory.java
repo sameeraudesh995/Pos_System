7967eea9a9bb15f2d7a2e694c52265cf66c96af5
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-09 18:06:58 +0530

    DAOFactory created

--- src/dao/DAOFactory.java
+++ src/dao/DAOFactory.java
@@ -1,4 +1,35 @@
 package dao;
 
+import dao.custom.LoginDAO;
+import dao.custom.impl.LoginDAOImpl;
+
 public class DAOFactory {
+
+    public static DAOFactory daoFactory;
+
+    public enum DAOType{
+        LOGIN
+    }
+
+    public static DAOFactory getDaoFactory(){
+        if(daoFactory == null) {
+            return daoFactory = new DAOFactory();
+
+        }else{
+            return daoFactory;
+        }
+    }
+
+    public LoginDAO getDAO(DAOType daoType){
+        switch (daoType){
+            case LOGIN:
+                return new LoginDAOImpl();
+            default:
+                return null;
+        }
+
+    }
+    private DAOFactory(){
+
+    }
 }
