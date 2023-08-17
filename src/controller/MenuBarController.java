b5ee2c31ce2095f191019678a133bb53dc6d72db
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-17 09:31:31 +0530

    set time on MenuBar.fxml

--- src/controller/MenuBarController.java
+++ src/controller/MenuBarController.java
@@ -1,10 +1,16 @@
 package controller;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 import util.ObjectPasser;
 
 import java.net.URL;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 public class MenuBarController implements Initializable {
@@ -12,6 +18,7 @@ public class MenuBarController implements Initializable {
     public Label txtUserFullName;
     public Label lblTime;
     public Label lblDate;
+    public LocalTime currentTime;
 
     public void initialize(){
 
@@ -20,6 +27,17 @@ public class MenuBarController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         txtUserFullName.setText(ObjectPasser.userFullName);
-    }
+        startClock();    }
+    private void startClock(){
+        Timeline timeline = new Timeline(new KeyFrame(Duration.ZERO, e -> {
+            currentTime = LocalTime.now();
+            lblTime.setText(currentTime.format(DateTimeFormatter.ofPattern("hh:mm:ss a")));
+        }),
+                new KeyFrame(Duration.seconds(1)));
 
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+
+
+    }
 }
