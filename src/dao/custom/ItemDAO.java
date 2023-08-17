d43e068de12aa21e5994901d9c959f413a9a1788
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-17 19:37:11 +0530

    item dao created

---
+++ src/dao/custom/ItemDAO.java
@@ -0,0 +1,8 @@
+package dao.custom;
+
+import entity.Item;
+
+public interface ItemDAO {
+
+    public boolean save(Item item);
+}
---
+++ src/dao/custom/impl/ItemDAOImpl.java
@@ -0,0 +1,28 @@
+package dao.custom.impl;
+
+import dao.CrudUtil;
+import dao.custom.ItemDAO;
+import entity.Item;
+
+import java.sql.SQLException;
+
+public class ItemDAOImpl implements ItemDAO {
+    @Override
+    public boolean save(Item item) {
+        try {
+            return CrudUtil.executeUpdate("INSERT INTO items VALUES(?,?,?,?,?,?,?,?)",
+                    item.getItemID(),
+                    item.getItemNane(),
+                    item.getBatchNumber(),
+                    item.getPrice(),
+                    item.getQty(),
+                    item.getSupplier(),
+                    item.getExpireDate()
+                    );
+        } catch (SQLException | ClassNotFoundException throwables){
+            throwables.printStackTrace();
+
+        }
+        return true;
+    }
+}
